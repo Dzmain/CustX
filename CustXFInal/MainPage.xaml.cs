716fe7c69b704d04cb481fc844e9a08ca1c05983
Dzmain <44649045+Dzmain@users.noreply.github.com>
2019-04-08 19:49:21 -0500

    Add files via upload

---
+++ CustXFInal/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CustXFInal
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        int count1 = 0;
+        int count2 = 0;
+        private async void TU_Clicked(object sender, EventArgs e)
+        {
+            count1++;
+            await Navigation.PushAsync(new Page2());
+
+        }
+        private async void TD_Clicked(object sender, EventArgs e)
+        {
+            count2++;
+            await Navigation.PushAsync(new TDF2());
+
+        }
+    }
+}
